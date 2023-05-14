31b12e837397f88c7898b4dbc0b4baadd7b885fb
mskzuk <ukai@root7.jp>
2023-05-14 18:29:53 +0900

    impl test environment

---
+++ models/account_test.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"bank-app/utils"
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    utils.RandomOwner(),
+		Balance:  utils.RandomInt(0, 100),
+		Currency: utils.RandomCurrency(),
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+}
---
+++ models/main_test.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect db: ", err)
+	}
+
+	testQueries = New(conn)
+	os.Exit(m.Run())
+}
